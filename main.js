31f153a5817c50ff08796430954ecfca20549bfe
itzelgress <141992384+itzelgress@users.noreply.github.com>
2023-08-11 00:08:22 -0600

    initial commite

---
+++ main.js
@@ -0,0 +1,9 @@
+function muestra_oculta(id) {
+    let dic= document.getElementById(id)
+    if (dispatchEvent.style.display= "none-2) {
+        div.style.display= "flex"
+    }
+    else{ 
+        div.style.display= "none"
+    }
+}
